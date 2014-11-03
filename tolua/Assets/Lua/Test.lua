bb728335af96998f9c0213eec1dd7541aa7042a8
topameng <topameng@gmail.com>
2014-11-03 12:20:07 +0800

    1.4

--- tolua/Assets/Lua/Test.lua
+++ tolua/Assets/Lua/Test.lua
@@ -1,4 +1,4 @@
-
+--测试极端条件性能
 function Test(transform)
 	local t = Time.realtimeSinceStartup;
 	local v = Vector3.one
@@ -10,18 +10,7 @@ function Test(transform)
 	print("lua cost time: " .. (Time.realtimeSinceStartup - t));
 end
 
-
-local curve = AnimationCurve.New(Keyframe.New(1,2), Keyframe.New(1,2))
-
-local Test = TestToLua.New("hello")
-local Test1 = TestToLua.New("hello", "123")
-Test:Test4(4, myFunc, 123)
-
---local co = coroutine.create(myFunc)
---local v, ret = coroutine.resume(co)
---print(tostring(v)..ret)
---v, ret = coroutine.resume(co)
-
+--测试操作符函数
 print("Test Vector3 operator func")
 local v1 = Vector3.New(1,2,3)
 v1 = v1 + Vector3.one
@@ -30,7 +19,7 @@ print(v1)
 print("The enum class TestEnum.Two is:" .. TestEnum.Two)
 
 
-
+--测试协同
 function TestCo()
 	print("current time:"..Time.time)
 	coroutine.waitforseconds(1)
