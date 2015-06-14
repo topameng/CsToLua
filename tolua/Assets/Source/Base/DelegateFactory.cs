3d55caf4c688490da49f4209cbc9868cd3718d51
topameng <topameng@gmail.com>
2015-06-14 21:02:38 +0800

    1.9.9 add delegate examples

---
+++ tolua/Assets/Source/Base/DelegateFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using LuaInterface;
+
+public static class DelegateFactory
+{
+	delegate Delegate DelegateValue(LuaFunction func);
+	static Dictionary<Type, DelegateValue> dict = new Dictionary<Type, DelegateValue>();
+
+	[NoToLuaAttribute]
+	public static void Register(IntPtr L)
+	{
+		dict.Add(typeof(Action<GameObject>), new DelegateValue(Action_GameObject));
+	}
+
+	[NoToLuaAttribute]
+	public static Delegate CreateDelegate(Type t, LuaFunction func)
+	{
+		DelegateValue create = null;
+
+		if (!dict.TryGetValue(t, out create))
+		{
+			Debugger.LogError("Delegate {0} not register", t.FullName);
+			return null;
+		}
+		return create(func);
+	}
+
+	public static Delegate Action_GameObject(LuaFunction func)
+	{
+		Action<GameObject> d = (arg0) =>
+		{
+			int top = func.BeginPCall();
+			IntPtr L = func.GetLuaState();
+			LuaScriptMgr.Push(L, arg0);
+			func.PCall(top, 1);
+			func.EndPCall(top);
+		};
+		return d;
+	}
+
+}
